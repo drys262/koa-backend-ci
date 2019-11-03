951eaf7c7774e6a92b76f1933f2fa5bec33ee8d5
drys262 <koeichavez17@gmail.com>
2019-11-04 04:07:41 +0800

    feat: initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:10
+
+WORKDIR /usr/src/app
+COPY package*.json ./
+
+# Install production dependencies.
+RUN npm install --only=production
+
+# Copy local code to the container image.
+COPY . .
+
+# Run the web service on container startup.
+CMD [ "npm", "start" ]
---
+++ app.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const logger = require('koa-logger');
+const cors = require('koa-cors');
+const app = new Koa();
+const router = new Router();
+const port = process.env.PORT || 80;
+
+router.get('/users', (ctx, next) => {
+  ctx.body = [
+    {
+      id: '1',
+      name: 'First user'
+    },
+    {
+      id: '2',
+      name: 'Second user'
+    },
+    {
+      id: '3',
+      name: 'Third user'
+    }
+  ];
+});
+
+app.use(cors());
+app.use(logger());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(port, () => console.log(`SERVER STARTED ON PORT ${port}`));
